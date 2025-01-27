8a6cab87f3090823c24bed20b5cad5f47f05d305
Malinda Prabath <malindaprabath876@gmail.com>
2025-01-27 23:53:21 +0530

    Completed fontend Development part

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,43 +1,47 @@
-import React from 'react'
+import { useState } from 'react'
 import './sidebar.css'
 
 
 const Sidebar = () => {
+    const [extended, setExtended] = useState(false)
     return (
-                <div className="sidebar">
-                    <div className="top">
-                        <img className='menu' src="./menu_icon.png" alt="" />
-                        <div className="new-chat">
-                            <img src="./plus_icon.png" alt="" />
-                            <p>New Chat</p>
-                        </div>
-                        <div className="recent">
-                            <p className="recent-title">Recent</p>
-                            <div className="recent-entry">
-                                <img src="./message_icon.png" alt="" />
-                                <p>What is react ...</p>
-                            </div>
-                        </div>
-                        
-                    </div>
-                    <div className="bottom">
-                        <div className="bottom-item recent-entry">
-                            <img src="./question_icon.png" alt="" />
-                            <p>Help</p>
-                        </div>
-
-                        <div className="bottom-item recent-entry">
-                            <img src="./history_icon.png" alt="" />
-                            <p>Activity</p>
-                        </div>
-
-                        <div className="bottom-item recent-entry">
-                            <img src="./setting_icon.png" alt="" />
-                            <p>Settings</p>
-                        </div>
-                    </div>
-                    
+        <div className="sidebar">
+            <div className="top">
+                <img onClick={()=>setExtended(prev=>!prev)} className='menu' src="./menu_icon.png" alt="" />
+                <div className="new-chat">
+                    <img src="./plus_icon.png" alt="" />
+                   {extended? <p>New Chat</p>: null}
                 </div>
+                {extended ?
+                 <div className="recent">
+                    <p className="recent-title">Recent</p>
+                    <div className="recent-entry">
+                        <img src="./message_icon.png" alt="" />
+                        <p>What is react ...</p>
+                    </div>
+                </div> : null
+                }
+               
+                        
+        </div>
+        <div className="bottom">
+             <div className="bottom-item recent-entry">
+                 <img src="./question_icon.png" alt="" />
+                 {extended ?<p>Help</p> : null}
+            </div>
+
+                <div className="bottom-item recent-entry">
+                    <img src="./history_icon.png" alt="" />
+                    {extended ?<p>Activity</p> : null}
+                </div>
+
+                <div className="bottom-item recent-entry">
+                     <img src="./setting_icon.png" alt="" />
+                     { extended ?<p>Settings</p> : null}
+                </div>
+        </div>
+                    
+     </div>
   )
 }
 
