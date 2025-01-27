341cfe90ee402be3b26e9a0aabf7cfc00de8e8cc
Malinda Prabath <malindaprabath876@gmail.com>
2025-01-27 19:26:05 +0530

    ims error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Sidebar from './components/sidebar/Sidebar'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+    <Sidebar />
+    </>
   )
 }
 
