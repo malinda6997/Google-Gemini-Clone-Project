fb126a92b10663d1643c5987e9e1ac4d889f6c99
malinda6997 <malindaprabath876@gmail.com>
2025-11-07 20:04:41 +0530

    Fix assets import paths - resolve build error
    - Fixed import paths in assets.js to reference public folder correctly
    - Changed from ES6 imports to direct public path strings
    - Resolved 'Failed to resolve import' error for all icon files
    - Assets now correctly point to ./filename.png in public directory

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,33 +1,18 @@
-import history_icon from './history_icon.png'
-import menu_icon from './menu_icon.png'
-import plus_icon from './plus_icon.png'
-import question_icon from './question_icon.png'
-import setting_icon from './setting_icon.png'
-import bulb_icon from './bulb_icon.png'
-import compass_icon from './compass_icon.png'
-import gallery_icon from './gallery_icon.png'
-import mic_icon from './mic_icon.png'
-import user_icon from './user_icon.png'
-import youtube_icon from './youtube_icon.png'
-import message_icon from './message_icon.png'
-import code_icon from './code_icon.png'
-import send_icon from './send_icon.png'
-import gemini_icon from './gemini_icon.png'
-
+// Assets from public folder - accessed via public paths
 export const assets = {
-    history_icon,
-    menu_icon,
-    plus_icon,
-    question_icon,
-    setting_icon,
-    bulb_icon,
-    compass_icon,
-    gallery_icon,
-    mic_icon,
-    user_icon,
-    youtube_icon,
-    message_icon,
-    code_icon,
-    send_icon,
-    gemini_icon
-}
+  history_icon: "./history_icon.png",
+  menu_icon: "./menu_icon.png",
+  plus_icon: "./plus_icon.png",
+  question_icon: "./question_icon.png",
+  setting_icon: "./setting_icon.png",
+  bulb_icon: "./bulb_icon.png",
+  compass_icon: "./compass_icon.png",
+  gallery_icon: "./gallery_icon.png",
+  mic_icon: "./mic_icon.png",
+  user_icon: "./me.jpg", // Note: using me.jpg as user icon
+  youtube_icon: "./youtube_icon.png",
+  message_icon: "./message_icon.png",
+  code_icon: "./code_icon.png",
+  send_icon: "./send_icon.png",
+  gemini_icon: "./gemini_icon.png",
+};
